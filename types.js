5cf65bee0850d1fd9e96dc9284a3f812bc874c6e
ajay1931 <ajayprakash032@gmail.com>
2024-06-18 06:30:57 +0530

    primitive and reference type init

---
+++ types.js
@@ -0,0 +1,23 @@
+//primitive type
+
+let x = 10;
+
+let y = x;
+
+x = 20;
+
+console.log(x);
+console.log(y);
+
+//reference type
+
+let a = {
+    value: 10
+};
+
+let b = a;
+
+a.value =20;
+
+console.log(a);
+console.log(b);
