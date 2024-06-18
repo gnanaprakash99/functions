937edc56d7e01c7838174e6e548c1dbc8c6e7d92
ajay1931 <ajayprakash032@gmail.com>
2024-06-18 13:12:50 +0530

    add some examples

--- types.js
+++ types.js
@@ -9,6 +9,18 @@ x = 20;
 console.log(x);
 console.log(y);
 
+//eg-2
+
+let car =5;
+
+function updateCar(car){
+    car++;
+    console.log(car);
+}
+updateCar(car)
+console.log(car);
+
+
 //reference type
 
 let a = {
@@ -21,3 +33,14 @@ a.value =20;
 
 console.log(a);
 console.log(b);
+
+//eg-2
+
+let cars ={value:5};
+
+function updateCar(cars){
+    cars.value++;
+    console.log(cars);
+}
+updateCar(cars)
+console.log(cars);
