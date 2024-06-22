e3b2f89b81eacbf68604b84889d0896acfc28618
ajay1931 <ajayprakash032@gmail.com>
2024-06-22 07:59:23 +0530

    using template literals

---
+++ template_literals.js
@@ -0,0 +1,21 @@
+let message = "Hi, i'm Ajay. I'm a software engineer working with my laptop."
+let message1 = "Hi, i'm Ajay.\n I'm a software engineer working with my laptop."
+let message2 = "Hi, i'm Ajay.\t I'm a software engineer working with my laptop."
+let message3 = `Hi, i'm Ajay. I'm a "software engineer" working with my laptop.`
+let message4 = `Hi, i'm Ajay.
+ I'm a "software engineer"
+ 
+ working with my laptop.`
+
+// on message3 and message4 using templte literals
+
+// console.log(message);
+// console.log(message1);
+// console.log(message2);
+// console.log(message3);
+// console.log(message4);
+
+
+let name ="Ajay" 
+let message5 = `Hi, i'm ${name}. I'm a "software engineer" working with my laptop.`
+console.log(message5);
